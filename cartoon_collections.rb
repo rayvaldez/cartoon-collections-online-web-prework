f43ebf95b727f0c399170ea0b02cedd8967eb7c1
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-14 17:53:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(names)
   names.map { |name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |word| word.length > 4
 end
 
 def find_the_cheese# code an argument here
